9d52a4b6ba7825c75b8942609177cd1e5faa6cc3
Robinson Severo <robinsonvs@gmail.com>
2018-05-17 07:34:58 -0300

    thread loop

--- src/main/java/br/com/digitec/analise/dados/venda/Main.java
+++ src/main/java/br/com/digitec/analise/dados/venda/Main.java
@@ -1,14 +1,25 @@
 package br.com.digitec.analise.dados.venda;
 
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 import br.com.digitec.analise.dados.venda.starter.Starter;
 
 public class Main 
 {
     public static void main( String[] args )
     {
-        Starter starter = new Starter();
-        while(true) {
-        	starter.executar();
-        }
+        executar();  
     }
+
+	private static void executar() {
+		Starter starter = new Starter();
+        ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(3);
+        executor.scheduleWithFixedDelay(new Runnable() {
+                  public void run() {
+                	  starter.executar();
+                	  System.out.println("passei");
+                  }
+            }, 0, 2000, TimeUnit.MILLISECONDS);
+	}
 }
